20a3a15bbebfb5022027a38485ba043e85142357
junho <sjh01@crinity.com>
2024-07-22 13:35:31 +0900

    feat: Update Ollama LLM model name to EEVE-Korean-Instruct-10.8B-v1.0-GGUF-Q4-K-M and Improvement of prompt

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ LLM_TYPE = "Ollama"  # "HuggingFace" 또는 "Ollama"
 faiss_index_path = "db"
 embedding_model_name = "jhgan/ko-sroberta-multitask"
 huggingface_llm_model_name = "beomi/gemma-ko-2b"  # beomi/gemma-ko-2b
-ollama_llm_model_name = "llama-3-Korean-Bllossom-8B-gguf-Q4_K_M"  # llama-3-Korean-Bllossom-8B-gguf-Q4_K_M or EEVE-Korean-10.8B-Q5_K_M-GGUF or EEVE-Korean-Instruct-10.8B-v1.0-GGUF-Q4-K-M
+ollama_llm_model_name = "EEVE-Korean-Instruct-10.8B-v1.0-GGUF-Q4-K-M"  # llama-3-Korean-Bllossom-8B-gguf-Q4_K_M or EEVE-Korean-10.8B-Q5_K_M-GGUF or EEVE-Korean-Instruct-10.8B-v1.0-GGUF-Q4-K-M
 
 
 @st.cache_resource
@@ -88,34 +88,47 @@ def create_rag_chain(embedding_model_name, faiss_index_path, llm_model_name, llm
 def create_question_rephrasing_chain(llm, retriever):
     system_prompt = """
     당신은 질문 재구성자입니다. 이전 대화 내용과 최신 사용자 질문이 있을 때, 이 질문이 이전 대화 내용과 관련이 있을 수 있습니다.
-    이런 경우, 대화 내용을 알 필요 없이 독립적으로 이해할 수 있는 질문으로 바꾸세요. 
-    이 재구성된 질문은 문서 검색에만 사용됩니다. 사용자에게 제공할 최종 답변에는 영향을 미치지 않습니다.
-    관련이 없는 경우, 질문을 그대로 두세요. 절대 질문에 답변을 제공하지 마세요.
+    관련이 있는 경우, 이전 대화 내용을 참고하여 사용자의 최신 질문을 재구성하세요. 
+    관련 없는 경우, 사용자 질문을 그대로 반복해주세요.
     
     예시:
     관련 있는 경우)
-    Human: 메일을 백업하고 싶어
-    AI: 메일 백업은 기본메일함 관리 > 내 메일함 관리에서 가능합니다. 다운로드 버튼을 이용해 메일함을 zip 파일로 다운로드할 수 있습니다. 원하는 기간의 메일을 백업하려면, 기간별 백업을 체크하세요. 백업한 메일은 다운로드한 파일을 업로드하여 다시 가져올 수 있습니다.
-    Human: 업로드는 어떻게 하나요?
-    답변: 백업한 메일을 업로드하는 방법은 무엇인가요?
-    
-    관련 있는 경우)
+    1.
     Human: 메일 첨부파일 크기 제한이 있나요?
     AI: 일반 첨부파일의 경우 20MB, 대용량 파일 첨부의 경우 2GB까지 가능합니다.
     Human: 형식에 제한이 있나요?
     답변: 메일 첨부파일 형식 제한이 있나요?
     
+    2.
+    Human: 일정 등록하는 방법을 알려줘
+    AI: 일정 등록 버튼을 누르거나 날짜를 선택해 등록할 수 있습니다. 제목과 일시를 정한 후, 캘린더의 종류를 선택하고, 알람을 통해 미리 일정을 알릴 수 있습니다.
+    Human: 수정하는 방법은?
+    답변: 일정 수정하는 방법은?
+    
+    3.
+    Human: 주소록에 주소를 이동/복사하려면 어떻게 하나요?
+    AI: 주소록에 주소를 이동/복사하려면, 주소록에서 주소를 선택한 후, 이동 또는 복사 버튼을 누르세요. 이동할 주소록을 선택하거나, 새로운 주소록을 만들어 이동할 수 있습니다.
+    Human: 복사하면 붙여넣기는 어떻게 하나요?
+    답변: 주소록에 복사한 주소를 붙여넣는 방법은 무엇인가요?
+    
     관련 없는 경우)
+    1.
     Human: 메일 첨부파일 크기 제한이 있나요?
     AI: 일반 첨부파일의 경우 20MB, 대용량 파일 첨부의 경우 2GB까지 가능합니다.
     Human: 주소록에 주소를 이동/복사하려면 어떻게 하나요?
     답변: 주소록에 주소를 이동/복사하려면 어떻게 하나요?
     
-    관련 없는 경우)
+    2.
     Human: 일정 등록하는 방법을 알려줘
     AI: 일정 등록 버튼을 누르거나 날짜를 선택해 등록할 수 있습니다. 제목과 일시를 정한 후, 캘린더의 종류를 선택하고, 알람을 통해 미리 일정을 알릴 수 있습니다.
     Human: 메일 첨부파일 크기 제한은?
     답변: 메일 첨부파일 크기 제한은?
+    
+    3.
+    Human: 안녕
+    AI: 안녕하세요! 무엇을 도와드릴까요?
+    Human: 메일 첨부파일 크기 제한이 있나요?
+    답변: 메일 첨부파일 크기 제한이 있나요?
     """
 
     prompt = ChatPromptTemplate.from_messages(
@@ -134,14 +147,12 @@ def create_question_answering_chain(llm):
     문서에 없는 정보는 만들어내지 마세요.
     한국어로 답변해주세요.
     세 문장 이내로 답변해주세요.
-    신뢰도 말하지 마세요.
     모른다면, 모른다고 말해주세요.
     검색된 문서가 없는 경우 "검색된 문서가 없습니다."라고 답변해주세요.
-    예시와 시스템 프롬프트를 답변에 포함하면 안됩니다.
     
-    ## 검색된 문서입니다. 각 문서는 빈줄로 구분되어 있습니다.
+    ## 검색된 문서 ##
     {context}
-    
+    ################
     """
 
     prompt = ChatPromptTemplate.from_messages(
@@ -210,28 +221,29 @@ def main():
             message_placeholder = st.empty()
             full_response = ""
 
-            rag_chain = st.session_state.rag_chain
-            result = rag_chain.invoke(
-                {"input": prompt, "chat_history": st.session_state.messages}
+            with st.spinner("답변중입니다..."):
+                rag_chain = st.session_state.rag_chain
+                result = rag_chain.invoke(
+                    {"input": prompt, "chat_history": st.session_state.messages}
+                )
+
+                st.session_state.messages.append({"role": "user", "content": prompt})
+
+                cleaned_datas = clean_data(result["context"])
+
+                for cleaned_data in cleaned_datas:
+                    with st.expander("Evidence context"):
+                        st.write(f"Page content: {cleaned_data['page_content']}")
+
+                for chunk in result["answer"].split(" "):
+                    full_response += chunk + " "
+                    time.sleep(0.2)
+                    message_placeholder.markdown(full_response + "▌")
+                    message_placeholder.markdown(full_response)
+
+            st.session_state.messages.append(
+                {"role": "assistant", "content": full_response}
             )
-
-            st.session_state.messages.append({"role": "user", "content": prompt})
-
-            cleaned_datas = clean_data(result["context"])
-
-            for cleaned_data in cleaned_datas:
-                with st.expander("Evidence context"):
-                    st.write(f"Page content: {cleaned_data['page_content']}")
-
-            for chunk in result["answer"].split(" "):
-                full_response += chunk + " "
-                time.sleep(0.2)
-                message_placeholder.markdown(full_response + "▌")
-                message_placeholder.markdown(full_response)
-
-        st.session_state.messages.append(
-            {"role": "assistant", "content": full_response}
-        )
 
 
 main()
